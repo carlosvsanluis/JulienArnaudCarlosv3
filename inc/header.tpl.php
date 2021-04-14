f82bd827d477bc0ef67159bd6c8b129502d8f6b2
lemorvanjulien35 <70151158+lemorvanjulien35@users.noreply.github.com>
2021-04-14 15:40:10 +0200

    test1

--- inc/header.tpl.php
+++ inc/header.tpl.php
@@ -7,7 +7,7 @@
   <title>Mezialib</title>
       <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+  <link rel="stylesheet" href="/css/style.css" media="screen" type="text/css" />
       <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -22,7 +22,7 @@
             <li <?php if ($currentPage == 'mesLivres') {echo ' id="en-cours"';} ?>class="left__nav-item left__nav-link:hover"><a href="privatespace.php?page=mesLivres" class="left__nav-link">Mes Livres</a></li>
             <li <?php if ($currentPage == 'mesEmprunts') {echo ' id="en-cours"';} ?>class="left__nav-item left__nav-link:hover"><a href="privatespace.php?page=mesEmprunts" class="left__nav-link">Mes emprunts</a></li>
             <li <?php if ($currentPage == 'profil') {echo ' id="en-cours"';} ?>class="left__nav-item left__nav-link:hover"><a href="privatespace.php?page=profil" class="left__nav-link">Profil</a></li>
-            <li <?php if ($currentPage == 'déconnexion') {echo ' id="en-cours"';} ?>class="left__nav-item left__nav-link:hover"><a href="privatespace.php?page=déconnexion" class="left__nav-link">Déconnexion</a></li>
+            <li <?php if ($currentPage == 'deconnexion') {echo ' id="en-cours"';} ?>class="left__nav-item left__nav-link:hover"><a href="privatespace.php?page=deconnexion" class="left__nav-link">Déconnexion</a></li>
           </ul>
         </nav>
       </header>
